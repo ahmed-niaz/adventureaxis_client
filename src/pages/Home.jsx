9c440abbb43a19f730c0d2e40bf151c705f45b73
ahmed-niaz <niazahmed.an@gmail.com>
2024-04-29 14:50:56 +0600

    create why adventure axis section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,43 @@ const Home = () => {
           <TouristSpots key={spot._id} spot={spot} />
         ))}
       </div>
+      {/* why people choose us */}
+      <div className="container mx-auto">
+        <p className="font-extrabold text-4xl text-center text-[#165D66]">Why Adventure Axis?</p>
+        <div className="divider w-9/12 mx-auto"></div>
+        <div className="grid lg:grid-cols-3 gap-4 w-full">
+          <div className=" space-y-2 my-8 py-14 bg-gray-50 rounded-2xl">
+            <div className="flex flex-col items-center justify-center text-[#165D66] font-extrabold">
+            <span className="text-3xl">  upTo </span>
+              <span className="text-9xl">30%</span>
+              <span className="text-4xl">discount</span>
+            </div>
+            <div className="w-9/12 mx-auto">
+              <p className="text-center text-gray-700 font-bold">For over a years offering the best plans at the best price with exclusive discounts.</p>
+            </div>
+          </div>
+          <div className=" space-y-2 my-8 py-14 bg-gray-50 rounded-2xl w-full">
+            <div className="flex flex-col items-center justify-center text-[#165D66] font-extrabold">
+            <span className="text-3xl">  over </span>
+              <span className="text-8xl">1 Million</span>
+              <span className="text-4xl">Reviews</span>
+            </div>
+            <div className="w-9/12 mx-auto">
+              <p className="text-center text-gray-700 font-bold">Browse and compare the views of users and safe acertarás.</p>
+            </div>
+          </div>
+          <div className=" space-y-2 my-8 py-14 bg-gray-50 rounded-2xl">
+            <div className="flex flex-col items-center justify-center text-[#165D66] font-extrabold">
+            <span className="text-3xl">  pays </span>
+              <span className="text-[90px]">the price + </span>
+              <span className="text-4xl">Low</span>
+            </div>
+            <div className="w-9/12 mx-auto">
+              <p className="text-center text-gray-700 font-bold">All days Escape at the best price in offers.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,14 +1,10 @@
 import notFound from "../assets/banner/notFound.png";
-import Footer from "./../components/Footer";
+
 const PageNotFound = () => {
   return (
     <main>
-      <div className="min-h-[80vh] flex items-center justify-center">
+      <div className="min-h-screen flex items-center justify-center">
         <img src={notFound} alt="" />
-      </div>
-
-      <div>
-        <Footer />
       </div>
     </main>
   );
